ee0a0c9acd6767b0006ed5c0d4a4b478bc48be02
parsa-script <mihandoost.p@gmail.com>
2020-04-15 02:27:06 +0430

    add feature spring security

---
+++ src/main/java/ir/codefather/mongodemo/security/Config.java
@@ -0,0 +1,40 @@
+package ir.codefather.mongodemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+
+@EnableWebSecurity
+public class Config extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("1234")).roles("Admin");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().ignoringAntMatchers("/api/**")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**/favicon.ico", "/css/**", "/img/**", "/js/**").permitAll()
+                .antMatchers("/api/**", "/", "/page/**","/user/avatar/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutSuccessUrl("/");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
